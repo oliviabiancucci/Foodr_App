80fd048b80d779237dcac5bdcf7cda8319adac27
Olivia Biancucci <obiancuc@uoguelph.ca>
2024-03-06 13:13:12 -0500

    set up main page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import CookBook from './CookBook';
+import Main from './Main';
 
 import { registerRootComponent } from 'expo';
 import Recipe from './recipe';
 
 // TODO: temp data import
-import {recipes as cookbook} from './recipe_list.json';
+import { recipes as cookbook } from './recipe_list.json';
 
 function App() {
   return (
     <View style={styles.container}>
-      <Recipe recipe={cookbook[0]}/>
+      <View style={styles.navBar}>
+        <Text style={styles.navBarTitle}>Foodr</Text>
+      </View>
+      <Main recipe={cookbook[0]} />
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -24,5 +29,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  navBar: {
+    width: '100%',
+    height: 80,
+    backgroundColor: '#EB6F6F',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  navBarTitle: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
