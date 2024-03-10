6beda74515233642d3c28cc1b770e003c4758673
stoll <stoll@uoguelph.ca>
2024-03-09 21:19:36 -0500

    added state managemnet for favorites

--- app/(tabs)/cookbook.jsx
+++ app/(tabs)/cookbook.jsx
@@ -1,35 +1,52 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, ScrollView } from "react-native";
 //import * as data from './recipe_list.json';
 import { recipes as data } from "../recipe_list.json";
 import SingleRecipeRow from "../SingleRecipeRow";
 import { Link } from "expo-router";
-
-export default Page = () => {
+import { inject, observer } from 'mobx-react';
+import stringStore  from "../SavedRecipes";
+//inject('stringStore')(observer(({ stringStore }
+const CookBook = observer(() => {
+    const favList = stringStore.saved.map((item, index) => (
+        <Link key={(index + 1) * 100}
+        href={{pathname: "recipe/[id]",
+        params: {id: index},
+        }} style={styles.row}>
+            <SingleRecipeRow title={item.name} image={item.image} speed={item.speed} price={item.price}/>
+        </Link>
+    ));
     return (
-        <FlatList
-            data={data}
-            renderItem={({ item, index }) => {
-                return (
-                    <Link
-                        href={{
-                            pathname: "recipe/[id]",
-                            params: { id: index },
-                        }} style={styles.row}
-                    >
-                        <SingleRecipeRow
-                            title={item.recipeName}
-                            image={item.imageUrl}
-                            speed={item.cookingTime}
-                            price={item.amountOfIngredients}
-                        />
-                    </Link>
-                );
-            }}
-            keyExtractor={(item) => item.recipeName}
-            horizontal={false}
-        />
+        <ScrollView>{favList}</ScrollView>
     );
-};
+}
+);
+export default CookBook;
+// export default Page = inject('allSaved')(observer(({ allSaved }) => {
+//     return (
+//         <FlatList
+//             data={data}
+//             renderItem={({ item, index }) => {
+//                 return (
+//                     <Link
+//                         href={{
+//                             pathname: "recipe/[id]",
+//                             params: { id: index },
+//                         }} style={styles.row}
+//                     >
+//                         <SingleRecipeRow
+//                             title={item.recipeName}
+//                             image={item.imageUrl}
+//                             speed={item.cookingTime}
+//                             price={item.amountOfIngredients}
+//                         />
+//                     </Link>
+//                 );
+//             }}
+//             keyExtractor={(item) => item.recipeName}
+//             horizontal={false}
+//         />
+//     );
+// }));
 const styles = StyleSheet.create({
     row: {
         flexDirection:'row',
