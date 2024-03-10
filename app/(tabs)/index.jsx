995a1978399c63c310d13dc5761a14e091b51abb
stoll <stoll@uoguelph.ca>
2024-03-09 21:21:41 -0500

    just fixed code a lil

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'mobx-react';
-import StringStore from '../SavedRecipes'; // Correct path to where your store is defined
-import Swipe from '../swipe'; // Correct path to your main component
+import StringStore from '../SavedRecipes'; 
+import Swipe from '../swipe'; 
 
 const App = () => (
   <Provider stringStore={StringStore}>
