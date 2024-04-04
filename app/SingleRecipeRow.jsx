52ef022cfe2cd3def75f69b783a4ed9e9261f510
Olivia Biancucci <obiancuc@uoguelph.ca>
2024-04-04 09:54:51 -0400

    added ability to remove recipes

--- app/SingleRecipeRow.jsx
+++ app/SingleRecipeRow.jsx
@@ -1,57 +1,68 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
-export default function SingleRecipeRow({title, image, price, speed}){
-    return(
-        <View style={styles.row}>
-            <Image style={styles.im} source={{uri: image}}></Image>
-            <View style={styles.column}>
-                <Text style={styles.title}>{title}</Text>
-                <View style={styles.view}><Text style={styles.tag}>{price}</Text></View>
-                <View style={styles.view}><Text style={styles.tag}> {speed}</Text></View>
-            </View>
-            {/* <FontAwesome style={styles.calIcon} name="calendar-plus-o" size={48} color="black" /> */}
-            {/* <Text>Calendar</Text> */}
-        </View>
-    )
+import recipeStore from "./SavedRecipes";
+
+export default function SingleRecipeRow({ title, image, price, speed, id}) {
+    const handleRemove = (id) => {
+        console.log("remove");
+        recipeStore.removeFavourite(id);
+      };
+    
+    return (
+    <View style={styles.row}>
+      <Image style={styles.im} source={{ uri: image }} />
+      <View style={styles.column}>
+        <Text style={styles.title}>{title}</Text>
+        <View style={styles.view}><Text style={styles.tag}>{price}</Text></View>
+        <View style={styles.view}><Text style={styles.tag}> {speed}</Text></View>
+      </View>
+        {/* <FontAwesome style={styles.calIcon} name="calendar-plus-o" size={48} color="black" /> */}
+        {/* <Text>Calendar</Text> */}
+    <TouchableOpacity onPress={() => handleRemove(id)} style={styles.removeButton}>
+        <FontAwesome name="times-circle" size={24} color="#EB6F6F" />
+    </TouchableOpacity>
+
+    </View>
+  )
 }
+
 const styles = StyleSheet.create({
-    im: {
-        width: 75,
-        height: 75,
-        marginTop: 15,
-        marginLeft: 15,
-    },
-    row: {
-        flexDirection:'row',
-    },
-    column: {
-        flexDirection: 'column',
-        marginLeft: 20,
-        width: 150,
-    },
-    title: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginTop: 10,
-    },
-    view: {
-        backgroundColor: "#EB6F6F", 
-        borderRadius: 10,
-        marginBottom: 5,
-        paddingTop: 5,
-        paddingBottom:5,
-    },
-    tag: {
-        fontSize: 14,
-        textAlign: 'left',
-        marginLeft: 10,
-        marginRight: 10,
-        maxWidth: 150,
-    },
-    calIcon: {
-        //margin:25, 
-        position: 'absolute',
-        marginLeft: 270,
-        marginTop: 25,
-    }
+  im: {
+    width: 75,
+    height: 75,
+    marginTop: 15,
+    marginLeft: 15,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  column: {
+    flexDirection: 'column',
+    marginLeft: 20,
+    width: 150,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  view: {
+    backgroundColor: "#EB6F6F",
+    borderRadius: 10,
+    marginBottom: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  tag: {
+    fontSize: 14,
+    textAlign: 'left',
+    marginLeft: 10,
+    marginRight: 10,
+    maxWidth: 150,
+  },
+  removeButton: {
+    marginLeft: 80,
+    marginTop: 15,
+  },
 });
