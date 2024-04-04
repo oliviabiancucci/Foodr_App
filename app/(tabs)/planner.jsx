2f2eea9c4ddc32b188b6856cc8879406249f9ade
BraydenChumbley <bchumbley2001@gmail.com>
2024-04-04 11:18:12 -0400

    added headers to planner

--- app/(tabs)/planner.jsx
+++ app/(tabs)/planner.jsx
@@ -3,38 +3,62 @@ import styles from "app/styles";
 import { useRouter } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-const { View, Text, ScrollView } = require("react-native");
+const { View, Text, ScrollView, StyleSheet } = require("react-native");
 
 import plannerStore from "app/PlannerStore";
 import SingleRecipeRow from "app/SingleRecipeRow";
 import { observer } from "mobx-react";
 
+const PlannerBlock = ({ title, plan }) => {
+    const recipes = plan ? plan.recipes : [];
+
+    return (
+        <View style={{ marginBottom: 50 }}>
+            <Text style={stylesheet.title}>{title}</Text>
+            {recipes.length == 0 ? (
+                <Text>No recipes planned</Text>
+            ) : (
+                recipes.map((recipe, index) => (
+                    <SingleRecipeRow
+                        key={index}
+                        title={recipe.name}
+                        image={recipe.thumbnail}
+                    />
+                ))
+            )}
+        </View>
+    );
+};
+
 const Planner = observer(() => {
     const router = useRouter();
 
-    plannerStore.plan.forEach((v) => {
-        console.log(v.recipes);
-    });
+    const date = new Date();
+    const todayPlan = plannerStore.getPlansBetween(date, date);
+
+    date.setDate(date.getDate() + 1);
+    const tmrwPlan = plannerStore.getPlansBetween(date, date);
+
+    date.setDate(date.getDate() + 1);
+    const futurePlan = plannerStore.getPlansBetween(date, null);
 
     return (
         <>
             <View style={styles.container}>
                 <ScrollView>
-                    {plannerStore.plan.map((dayPlan, index) => {
-                        return (
-                            <>
-                                <Text>{dayPlan.date.toDateString()}</Text>
-                                {dayPlan.recipes.map((recipe, index) => {
-                                    return (
-                                        <SingleRecipeRow
-                                            key={index}
-                                            title={recipe.name}
-                                            image={recipe.thumbnail}
-                                        />
-                                    );
-                                })}
-                            </>
-                        );
+                    <PlannerBlock title="Today" plan={todayPlan[0]} />
+                    <PlannerBlock title="Tomorrow" plan={tmrwPlan[0]} />
+                    {futurePlan.map((p, idx) => {
+                        return <PlannerBlock
+                            key={idx}
+                            title={p.date.toLocaleDateString(undefined, {
+                                weekday: "long",
+                                year: "numeric",
+                                month: "long",
+                                day: "numeric",
+                            })}
+                            plan={p}
+                        />;
                     })}
                 </ScrollView>
             </View>
@@ -51,6 +75,12 @@ const Planner = observer(() => {
             />
         </>
     );
+});
+
+const stylesheet = StyleSheet.create({
+    title: {
+        fontSize: 24,
+    },
 });
 
 export default Planner;
--- app/PlannerStore.js
+++ app/PlannerStore.js
@@ -1,28 +1,54 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable } from "mobx";
 
 class PlannerStore {
-  plan = [];
+    plan = [];
 
-  constructor() {
-    makeAutoObservable(this);
-  }
-
-  addRecipe(recipe, date) {
-    plannedDay = this.plan.find(p => (p.date.getYear() == date.getYear() && p.date.getMonth() == date.getMonth() && p.date.getDate() == date.getDate()));
-
-    if(plannedDay) {
-        plannedDay.recipes.push(recipe);
-    }
-    else {
-        this.plan.push({
-            date: date,
-            recipes: [recipe]
-        })
-        this.plan.sort((a, b) => a.date - b.date);
+    constructor() {
+        makeAutoObservable(this);
     }
 
-  }
+    addRecipe(recipe, date) {
+        plannedDay = this.plan.find(
+            (p) =>
+                p.date.getYear() == date.getYear() &&
+                p.date.getMonth() == date.getMonth() &&
+                p.date.getDate() == date.getDate()
+        );
 
+        if (plannedDay) {
+            plannedDay.recipes.push(recipe);
+        } else {
+            this.plan.push({
+                date: date,
+                recipes: [recipe],
+            });
+            this.plan.sort((a, b) => a.date - b.date);
+        }
+    }
+
+    getPlansBetween(date1, date2) {
+        return this.plan.filter((p) => {
+            if (date1) {
+                // check plan is on or after date 1
+                if (
+                    p.date.getYear() < date1.getYear() ||
+                    p.date.getMonth() < date1.getMonth() ||
+                    p.date.getDate() < date1.getDate()
+                )
+                    return false;
+            }
+            if (date2) {
+                // check plan is before or on date 2
+                if (
+                    p.date.getYear() > date2.getYear() ||
+                    p.date.getMonth() > date2.getMonth() ||
+                    p.date.getDate() > date2.getDate()
+                )
+                    return false;
+            }
+            return true;
+        });
+    }
 }
 const plannerStore = new PlannerStore();
 export default plannerStore;
